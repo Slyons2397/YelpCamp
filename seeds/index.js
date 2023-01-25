0abd067f9f7349852c5dbcd0a07ce9231b03e621
SamLyons <SamLyons@Samuels-MacBook-Air.local>
2023-01-25 16:29:34 -0500

    Added image uploading

--- seeds/index.js
+++ seeds/index.js
@@ -22,7 +22,20 @@ const seedDB = async () => {
       author: "63d040de383e6232ef3b1480",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)}, ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
+      images: [
+        {
+          url: "https://res.cloudinary.com/diweqcalq/image/upload/v1674676982/YelpCamp/j0z3zwbc2klv2pt0crrm.jpg",
+          filename: "YelpCamp/j0z3zwbc2klv2pt0crrm",
+        },
+        {
+          url: "https://res.cloudinary.com/diweqcalq/image/upload/v1674676982/YelpCamp/pwkvp5mhvm5sdmsie2a0.jpg",
+          filename: "YelpCamp/pwkvp5mhvm5sdmsie2a0",
+        },
+        {
+          url: "https://res.cloudinary.com/diweqcalq/image/upload/v1674676983/YelpCamp/lfb4pfrlvus4afr3ofzz.jpg",
+          filename: "YelpCamp/lfb4pfrlvus4afr3ofzz",
+        },
+      ],
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, id nemo reiciendis quos nesciunt obcaecati cum iure eius fugiat dolore adipisci! Eaque placeat velit repellendus?",
       price,
