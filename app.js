bd8261adbc0b78f9c757bd1f24bb1218d08cbf8e
SamLyons <SamLyons@Samuels-MacBook-Air.local>
2023-01-25 19:27:26 -0500

    updated stylings

--- app.js
+++ app.js
@@ -60,12 +60,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/fakeuser", async (req, res) => {
-  const user = new User({ email: "colt@gmail.com", username: "Colt" });
-  const newUser = await User.register(user, "chicken");
-  res.send(newUser);
-});
-
 app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
